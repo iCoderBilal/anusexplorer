744f7123a297ba65816fc6f2b6e80778f13d26d1
anus-engineer <anus@uranus.army>
2024-03-28 22:13:40 +0000

    anus

--- src/components/Believe.jsx
+++ src/components/Believe.jsx
@@ -1,56 +1,83 @@
 function Believe() {
   return (
     <>
-      <div className="believe-section pb-20">
-        <h1 className="pt-16 lg:pt-32 text-[7vw] lg:text-[2.5vw] lg:pl-[250px] lg:pr-[250px] pl-3 pr-3 leading-[9vw] lg:leading-[3vw] text-center text-white font-bold">
-          In Crypto We Trust, But In Uranus, We Believe!
-        </h1>
-        <p className="mt-14 text-[4vw] lg:text-[1.2vw] lg:pl-[33vw] lg:pr-[33vw] pl-5 pr-5 text-slate-300 lg:leading-[1.5vw] text-left">
-          - Everyone Loves to Pump Uranus more then they like when Uranus Dumps.
-          <br />
-          <br />
-          - The liquidity injections just keep coming into Uranus.
-          <br />
-          <br />
-          - Don’t pass gas (it’s gonna be giant)
-          <br />
-          <br />
-          - Hold onto Uranus!
-          <br />
-          <br />- Disclaimer: This is all in good fun – a blend of memes,
-          astrology, and crypto. We will not go moon, we going to Uranus.
+      <div className="bg-gradient-to-br from-purple-800 to-blue-600 min-h-screen p-5 lg:p-10 text-white">
+  <div className="max-w-4xl mx-auto">
+    <h1 className="text-4xl lg:text-5xl font-bold text-center mb-10 underline">
+      Journey to Uranus: Acquire $ANUS
+    </h1>
+
+    <div className="space-y-10">
+      <div className="journey-step bg-indigo-900 bg-opacity-80 shadow-xl p-5 lg:p-8 rounded-lg">
+        <h2 className="text-3xl font-semibold mb-3">1. Prepare For Entry</h2>
+        <p>
+          Begin by visiting <a href="https://solflare.com/" className="underline text-blue-300 hover:text-blue-200" target="_blank" rel="noopener noreferrer">Solflare's website</a> to download the Solflare wallet. Follow the instructions to set up your wallet and securely store your seed phrase. If you prefer using a hardware wallet, Solflare supports connections with devices like Ledger.
         </p>
       </div>
 
+      <div className="journey-step bg-indigo-900 bg-opacity-80 shadow-xl p-5 lg:p-8 rounded-lg">
+        <h2 className="text-3xl font-semibold mb-3">2. Get Your Injection Ready</h2>
+        <p>
+          Fund Your Wallet with Solana (SOL): To engage in transactions on the Solana network, you'll need SOL, the native cryptocurrency, to cover transaction fees (gas). You can acquire SOL through centralized exchanges by trading fiat or other cryptocurrencies. Once you have SOL, withdraw it to your Solflare wallet address.
+        </p>
+        <p className="mt-4">
+          For those familiar with Metamask or Rabby, who may already have Ethereum consider using the <a href="https://core.allbridge.io/" className="underline text-blue-300 hover:text-blue-200" target="_blank" rel="noopener noreferrer">Allbridge Core bridge</a> for a seamless transfer experience.
+        </p>
+      </div>
+
+      <div className="journey-step bg-indigo-900 bg-opacity-80 shadow-xl p-5 lg:p-8 rounded-lg">
+        <h2 className="text-3xl font-semibold mb-3">3. Inject Your Liquidity Into Uranus</h2>
+        <p>
+          It’s time to use the app to swap your sol into Uranus. Make sure it's the right one – no one likes ending up on the wrong planet. Decide how many SOL units you want to exchange, and confirm the transaction in your wallet. It’s like swapping space fuel for cosmic goodies!
+        </p>
+        <p className="mt-4 font-mono text-sm lg:text-base bg-blue-900 p-1 rounded">
+          9hjZ8UTNrNWt3YUTHVpvzdQjNbp64NbKSDsbLqKR6BZc
+        </p>
+      </div>
+
+      <div className="journey-step bg-indigo-900 bg-opacity-80 shadow-xl p-5 lg:p-8 rounded-lg">
+        <h2 className="text-3xl font-semibold mb-3">4. Watch others pump Uranus</h2>
+        <p>
+          After your escapade, you might want to keep an eye on your intergalactic investment. Tools like <a href="https://www.dextools.io/app/en/solana/pair-explorer/your-token-pair" className="underline text-blue-300 hover:text-blue-200" target="_blank" rel="noopener noreferrer">DEX Screener</a> are your telescopes to the stars, helping you monitor the market movements and performance of your token.
+        </p>
+      </div>
+    </div>
+
+    <p className="mt-10 italic text-center">
+      The revolution is underway, you’re still early. Uranus: it’s more than a load of hot air. It’s the downfall of the false rulers of humanity 🙂
+    </p>
+  </div>
+</div>
+
+
       <div className="believe-video-section pb-20">
-        <h1 className="pt-16 lg:pt-32 text-[7vw] lg:text-[2.5vw] lg:pl-[15vw] lg:pr-[15vw] pl-3 pr-3 leading-[9vw] lg:leading-[3vw] text-center text-white font-bold">
-        The Story of The Shiba Inu Army Pumping Uranus
+        <h1 className="text-[6vw] lg:text-5xl xl:text-6xl 2xl:text-7xl lg:px-[15vw] px-4 text-center text-white font-bold">
+          The Story of The Shiba Inu Army Pumping Uranus
         </h1>
         <div className="flex justify-center mt-10">
           <iframe
-         data-aos="zoom-in"
+            data-aos="zoom-in"
             src="https://www.youtube.com/embed/-KJZCYW_pN8?si=Au1JpegZCtG3UJGg"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen
-            className="lg:w-[50vw] lg:h-[28vw] w-[90vw] h-[48vw]"
+            className="lg:w-[50vw] lg:h-[28vw] w-[90vw] h-[50vw] xl:h-[30vw]"
           ></iframe>
-          
         </div>
       </div>
 
-
       <div className="believe-section pb-20">
-        <h1 className="pt-16 lg:pt-32 text-[7vw] lg:text-[2.5vw] lg:pl-[250px] lg:pr-[250px] pl-3 pr-3 leading-[9vw] lg:leading-[3vw] text-center text-white font-bold">
-        The Memeifesto: Embrace the Wild Side of Crypto
+        <h1 className="text-[6vw] lg:text-5xl xl:text-6xl 2xl:text-7xl lg:px-[250px] px-4 leading-tight text-center text-white font-bold">
+          The Memeifesto: Embrace the Wild Side of Crypto
         </h1>
-        <p className="mt-8 mb-14 text-[4vw] lg:text-[1.2vw] lg:pl-[33vw] lg:pr-[33vw] pl-3 pr-3 text-slate-300 lg:leading-[1.5vw] text-center">
-        Welcome to the wild side of crypto - where memes aren't just dreams, and astrology isn’t just your daily horoscope.
+        <p className="mt-8 mb-14 text-[3.5vw] lg:text-lg xl:text-xl 2xl:text-2xl lg:px-[33vw] px-4 text-white text-center">
+          Welcome to the wild side of crypto - where memes aren't just dreams, and astrology isn’t just your daily horoscope.
         </p>
-
         
-        <a href="https://coinmarketcap.com/currencies/uranus-sol/" className="mx-auto text-center py-[15px] px-[40px] bg-custom-blue lg:text-[1.5vw] lg:leading-[2.2vw] font-semibold text-white rounded-[40px]">Get Started</a>
+        <a href="https://coinmarketcap.com/currencies/uranus-sol/" className="block mx-auto text-center py-3 px-8 bg-blue-500 text-lg lg:text-xl xl:text-2xl font-semibold text-white rounded-full hover:bg-blue-600 transition-colors duration-300 ease-in-out">
+          Get Started
+        </a>
       </div>
     </>
   );
