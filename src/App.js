7fab44be73177fc01399633f841ff58502c263fd
anus-engineering <anus.engineering@proton.me>
2024-03-28 23:10:51 -0500

    smooth scroll

--- src/App.js
+++ src/App.js
@@ -20,6 +20,22 @@ function App() {
       // Global settings like duration, once, etc.
       duration: 1200,
     });
+
+    const scrollToSectionFromHash = () => {
+      const hash = window.location.hash;
+      if (hash && believeRef.current && hash === "#how-to-buy") {
+        setTimeout(() => {
+          believeRef.current.scrollIntoView({ behavior: 'smooth' });
+        }, 500); // Adjust delay as needed
+      }
+    };
+
+    scrollToSectionFromHash();
+
+    window.addEventListener('hashchange', scrollToSectionFromHash);
+
+    // Cleanup the event listener on component unmount
+    return () => window.removeEventListener('hashchange', scrollToSectionFromHash);
   }, []);
   return (
     <div className="App">
